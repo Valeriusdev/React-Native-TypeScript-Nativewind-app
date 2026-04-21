4a92f9888f5df44ba52136803fcf56ba3daa6ee0
Val Lysenko <kakao757@gmail.com>
2026-04-21 13:32:43 -0700

    add SafeAreaView

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,20 @@
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Link href="/about">About page</Link>
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-between",
     alignItems: "center",
     backgroundColor: "lightblue",
   },
