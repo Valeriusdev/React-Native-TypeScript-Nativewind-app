e48b7bfb368a07891c0d054762149176589c4a42
Val Lysenko <kakao757@gmail.com>
2026-06-20 21:44:56 -0700

    filter user query to authenticated user and add friend ID input field

--- db/user.ts
+++ db/user.ts
@@ -1,7 +1,7 @@
 import { supabase } from "@/lib/supabase";
 
-export async function getUser() {
-  const { data, error } = await supabase.from("users").select("*").single();
+export async function getUser(id: string) {
+  const { data, error } = await supabase.from("users").select("*").eq("id", id).single();
 
   if (error) {
     throw new Error(error.message);
